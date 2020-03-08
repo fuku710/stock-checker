073cdfc674954cf7eea8d3faed511cace1aa7241
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2020-03-08 17:12:04 +0900

    add log

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import { CronJob } from 'cron'
+import { getLogger, configure } from 'log4js'
 import { checkStock, StockResult } from './check-stock'
 import { sendMessageToLine } from './notification'
 import { SiteConfig } from './config'
 import siteConfigJSON from './site-config.json'
 
-function runCheckingAllStock() {
-  const siteConfigList: SiteConfig[] = siteConfigJSON
-
-  siteConfigList.forEach(async siteConfig => {
-    const result: StockResult = await checkStock(siteConfig)
-    if (result.hasStock) {
-      const message = `在庫が存在する可能性があります[${result.config.name}]:${result.config.url}`
-      await sendMessageToLine(message)
-    }
-    console.log(result)
-  })
-}
+const siteConfigList: SiteConfig[] = siteConfigJSON
+const logger = getLogger()
+configure({
+  appenders: {
+    result: { type: 'file', filename: 'result.log' }
+  },
+  categories: {
+    default: { appenders: ['result'], level: 'info' }
+  }
+})
 
 async function main() {
-  const job: CronJob = new CronJob('* 0 * * * *', runCheckingAllStock)
+  const job: CronJob = new CronJob('* 0 * * * *', () => {
+    siteConfigList.forEach(async siteConfig => {
+      const result: StockResult = await checkStock(siteConfig)
+      if (result.hasStock) {
+        const message = `在庫が存在する可能性があります[${result.config.name}]:${result.config.url}`
+        await sendMessageToLine(message)
+      }
+      logger.info(JSON.stringify(result))
+    })
+  })
   job.start()
 }
 
