026296e8e47afaae0c241b30c409bfdb308af257
Fukuda Naoto <naoto.fukuda1217@gmail.com>
2020-03-08 21:54:40 +0900

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import siteConfigJSON from './site-config.json'
 
 const siteConfigList: SiteConfig[] = siteConfigJSON
 const logger = getLogger()
+const job: CronJob = new CronJob('0 0 * * * *', main)
+
 configure({
   appenders: {
     result: { type: 'file', filename: './logs/result.log' },
@@ -19,21 +21,18 @@ configure({
 })
 
 async function main() {
-  const job: CronJob = new CronJob('0 0 * * * *', () => {
-    siteConfigList.forEach(async siteConfig => {
-      try {
-        const result: StockResult = await checkStock(siteConfig)
-        if (result.hasStock) {
-          const message = `在庫が存在する可能性があります[${result.config.name}]:${result.config.url}`
-          await sendMessageToLine(message)
-        }
-        logger.info(JSON.stringify(result))
-      } catch (err) {
-        logger.error(err)
+  siteConfigList.forEach(async siteConfig => {
+    try {
+      const result: StockResult = await checkStock(siteConfig)
+      if (result.hasStock) {
+        const message = `在庫が存在する可能性があります[${result.config.name}]:${result.config.url}`
+        await sendMessageToLine(message)
       }
-    })
+      logger.info(JSON.stringify(result))
+    } catch (err) {
+      logger.error(err)
+    }
   })
-  job.start()
 }
 
-main()
+job.start()
